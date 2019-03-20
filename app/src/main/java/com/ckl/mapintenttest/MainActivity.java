f16889fbcc1e00dbc48863fe3f0c13976cdaac20
Callum <callum.leung@gmail.com>
2019-03-20 21:19:20 +0000

    Initial commit

---
+++ app/src/main/java/com/ckl/mapintenttest/MainActivity.java
@@ -0,0 +1,169 @@
+package com.ckl.mapintenttest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        createMap();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void createMap(){
+        WebView webview = (WebView) findViewById(R.id.webview1);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://www.google.com/maps/dir/?api=1" +
+                "&origin= 2 Drum Rd, Chester le Street, DH2 1AB" +
+                "&destination=Seaton Burn Services, Fisher Lane, Newcastle upon Tyne NE13 6BP" +
+                "&waypoints=Alnwick Town Centre, 19 Lagny Street, Alnwick, NE66 1LA" +
+                "|Newton Aycliffe, 4 Northfield Way, Newton Aycliffe, DL5 6EJ" +
+                "|Thirsk Town Centre, 26 Market Place, Thirsk YO7 1LB" +
+                "|Whitby Town Centre, Station Square, Whitby YO21 1DX");
+    }
+
+    public void seatonBurnDirections(View view){
+       /* Uri gmmIntentUri = Uri.parse("geo:54.851597, -1.572178");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }*/
+
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=Seaton Burn Services, Fisher Lane, Newcastle upon Tyne NE13 6BP+to: Alnwick Town Centre, 19 Lagny Street, Alnwick, NE66 1LA ");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+    public void alnwickTownDirections(View view){
+
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=Alnwick Town Centre, 19 Lagny Street, Alnwick, NE66 1LA ");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+
+    }
+    public void newtonAycliffeDirections(View view){
+
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=Alnwick Town Centre, 19 Lagny Street, Alnwick, NE66 1LA");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+
+    }
+    public void thirskTownDirections(View view){
+
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=Alnwick Town Centre, 19 Lagny Street, Alnwick, NE66 1LA");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+
+    }
+    public void whitbyTownDirections(View view){
+
+
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=Alnwick Town Centre, 19 Lagny Street, Alnwick, NE66 1LA");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+
+    }
+
+}
