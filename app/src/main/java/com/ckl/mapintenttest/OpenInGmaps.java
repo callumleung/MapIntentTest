6c2f0e5093f8b05347147a3a9325c2502db6b922
callumleung <callum.leung@gmail.com>
2019-03-26 10:00:35 +0000

    Added navigation menu to all activities

--- app/src/main/java/com/ckl/mapintenttest/OpenInGmaps.java
+++ app/src/main/java/com/ckl/mapintenttest/OpenInGmaps.java
@@ -2,17 +2,24 @@ package com.ckl.mapintenttest;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class OpenInGmaps extends AppCompatActivity {
+public class OpenInGmaps extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_in_gmaps);
-
+        super.onCreateDraw();
 
     }
 
@@ -76,5 +83,9 @@ public class OpenInGmaps extends AppCompatActivity {
         startActivity(mapIntent);
 
     }
+
+
+
+
 
 }
